5d2b1aca83ac9f60e223b25042f5644d656e5831
Katherine Bertelsen <kbertelsen@bitwarden.com>
2024-03-21 23:03:05 -0500

    So many things

---
+++ AuthenticatorShared/UI/Platform/Application/AppCoordinator.swift
@@ -0,0 +1,77 @@
+import BitwardenSdk
+import SwiftUI
+import UIKit
+
+// MARK: - AppCoordinator
+
+/// A coordinator that manages the app's top-level navigation.
+///
+class AppCoordinator: Coordinator, HasRootNavigator {
+    // MARK: Types
+
+    /// The types of modules used by this coordinator.
+    typealias Module = VaultModule
+
+    // MARK: Private Properties
+
+    /// The context that the app is running within.
+    private let appContext: AppContext
+
+    /// The coordinator currently being displayed.
+    private var childCoordinator: AnyObject?
+
+    // MARK: Properties
+
+    /// The module to use for creating child coordinators.
+    let module: Module
+
+    /// The navigator to use for presenting screens.
+    private(set) weak var rootNavigator: RootNavigator?
+
+    /// The service container used by the coordinator
+    private let services: Services
+
+    // MARK: Initialization
+
+    /// Creates a new `AppCoordinator`.
+    ///
+    /// - Parameters:
+    ///   - appContext: The context that the app is running within.
+    ///   - appExtensionDelegate: A delegate used to communicate with the app extension.
+    ///   - module: The module to use for creating child coordinators.
+    ///   - rootNavigator: The navigator to use for presenting screens.
+    ///   - services: The service container used by the coordinator.
+    ///
+    init(
+        appContext: AppContext,
+        module: Module,
+        rootNavigator: RootNavigator,
+        services: Services
+    ) {
+        self.appContext = appContext
+        self.module = module
+        self.rootNavigator = rootNavigator
+        self.services = services
+    }
+
+    // MARK: Methods
+
+    func handleEvent(_ event: AppEvent, context: AnyObject?) async {
+        switch event {
+        case .didStart:
+            break
+        }
+    }
+
+    func navigate(to route: AppRoute, context _: AnyObject?) {
+        switch route {
+        case .onboarding:
+            break
+        }
+    }
+
+    func start() {
+        // Nothing to do here - the initial route is specified by `AppProcessor` and this
+        // coordinator doesn't need to navigate within the `Navigator` since it's the root.
+    }
+}
---
+++ AuthenticatorShared/UI/Vault/Vault/VaultList/VaultListView.swift
@@ -0,0 +1,14 @@
+// swiftlint:disable file_length
+
+import BitwardenSdk
+import SwiftUI
+
+// MARK: - VaultListView
+
+/// A view that allows the user to view a list of the items in their vault.
+///
+struct VaultListView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
