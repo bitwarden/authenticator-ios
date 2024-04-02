0d9e5e4c013f5c5a16fbc43d1108bf470939c22e
Katherine Bertelsen <kbertelsen@bitwarden.com>
2024-04-01 21:40:45 -0500

    Hook up token processor to token coordinator

--- AuthenticatorShared/UI/Vault/Token/TokenCoordinator.swift
+++ AuthenticatorShared/UI/Vault/Token/TokenCoordinator.swift
@@ -11,6 +11,8 @@ class TokenCoordinator: NSObject, Coordinator, HasStackNavigator {
     typealias Module = TokenModule
 
     typealias Services = HasErrorReporter
+        & HasTimeProvider
+        & ViewTokenProcessor.Services
 
     // MARK: - Private Properties
 
@@ -48,7 +50,41 @@ class TokenCoordinator: NSObject, Coordinator, HasStackNavigator {
 
     func handleEvent(_ event: TokenEvent, context: AnyObject?) async {}
 
-    func navigate(to route: TokenRoute, context: AnyObject?) {}
+    func navigate(to route: TokenRoute, context: AnyObject?) {
+        switch route {
+        case let .alert(alert):
+            stackNavigator?.present(alert)
+        case let .dismiss(onDismiss):
+            stackNavigator?.dismiss(animated: true, completion: {
+                onDismiss?.action()
+            })
+        case let .viewToken(id):
+            showViewToken(id: id)
+        }
+    }
 
     func start() {}
+
+    // MARK: Private Methods
+
+    /// Shows the view token screen.
+    ///
+    /// - Parameters:
+    ///   - id: The id of the token to show.
+    ///   - delegate: The delegate.
+    ///
+    private func showViewToken(id: String) {
+        let processor = ViewTokenProcessor(
+            coordinator: asAnyCoordinator(),
+            itemId: id,
+            services: services,
+            state: ViewTokenState()
+        )
+        let store = Store(processor: processor)
+        let view = ViewTokenView(
+            store: store,
+            timeProvider: services.timeProvider
+        )
+        stackNavigator?.replace(view)
+    }
 }
--- AuthenticatorShared/UI/Vault/Token/ViewToken/ViewTokenProcessor.swift
+++ AuthenticatorShared/UI/Vault/Token/ViewToken/ViewTokenProcessor.swift
@@ -16,8 +16,8 @@ final class ViewTokenProcessor: StateProcessor<
 
     // MARK: Properties
 
-    /// The `Coordinator` that handles navigation, typically an `ItemsCoordinator`.
-    private let coordinator: AnyCoordinator<ItemListRoute, ItemListEvent>
+    /// The `Coordinator` that handles navigation, typically a `TokenCoordinator`.
+    private let coordinator: AnyCoordinator<TokenRoute, TokenEvent>
 
     /// The ID of the item being viewed.
     private let itemId: String
@@ -36,7 +36,7 @@ final class ViewTokenProcessor: StateProcessor<
     ///   - state: The initial state of this processor.
     ///
     init(
-        coordinator: AnyCoordinator<ItemListRoute, ItemListEvent>,
+        coordinator: AnyCoordinator<TokenRoute, TokenEvent>,
         itemId: String,
         services: Services,
         state: ViewTokenState
