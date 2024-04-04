b54d9af77382e255f3dd550f6605b98104f0b6da
Katherine Bertelsen <kbertelsen@bitwarden.com>
2024-04-04 12:59:50 -0500

    Delete vaultlistitem

--- AuthenticatorShared/Core/Vault/Services/TOTP/TOTPExpirationCalculator.swift
+++ AuthenticatorShared/Core/Vault/Services/TOTP/TOTPExpirationCalculator.swift
@@ -52,26 +52,6 @@ enum TOTPExpirationCalculator {
         return sortedItems
     }
 
-    /// Sorts a list of `VaultListItem` by expiration state
-    ///
-    /// - Parameters:
-    ///   - items: An array of list items that may be expired.
-    ///   - period: The interval after which the codes are expired.
-    ///   - timeProvider: The provider of the current time.
-    ///
-    /// - Returns: A dictionary with the items sorted by a `Bool` flag indicating expiration.
-    ///
-    static func listItemsByExpiration(
-        _ items: [VaultListItem],
-        timeProvider: any TimeProvider
-    ) -> [Bool: [VaultListItem]] {
-        let sortedItems: [Bool: [VaultListItem]] = Dictionary(grouping: items, by: { item in
-            guard case let .totp(_, model) = item.itemType else { return false }
-            return hasCodeExpired(model.totpCode, timeProvider: timeProvider)
-        })
-        return sortedItems
-    }
-
     /// Calculates the seconds remaining before an update is needed
     ///
     /// - Parameters:
