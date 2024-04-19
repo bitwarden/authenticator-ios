c47172aae5c2874c4cb679275e11361c0ef2f6b5
Katherine Bertelsen <kbertelsen@bitwarden.com>
2024-04-19 15:18:40 -0500

    Implement export at the UI level

---
+++ AuthenticatorShared/UI/Platform/Settings/Settings/ExportItems/ExportItemsProcessor.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+// MARK: - ExportItemsProcessor
+
+/// The processor used to manage state and handle actions for an `ExportItemsView`.
+final class ExportItemsProcessor {
+
+}
---
+++ AuthenticatorShared/UI/Platform/Settings/Settings/ExportItems/ExportItemsState.swift
@@ -0,0 +1,6 @@
+// MARK: - ExportItemsState
+
+/// The current state of an `ExportItemsView`.
+struct ExportItemsState: Equatable {
+    // MARK: Properties
+}
