245cc7403502916ac38a8d05c0be33a3c31ea1bf
Katherine Bertelsen <kbertelsen@bitwarden.com>
2024-04-19 14:22:00 -0500

    Create export items service

--- AuthenticatorShared/Core/Vault/Models/Domain/ExportFileType.swift
+++ AuthenticatorShared/Core/Vault/Models/Domain/ExportFileType.swift
@@ -3,17 +3,12 @@
 /// An enum describing the format of an export file.
 ///
 enum ExportFileType: Equatable {
-    /// A `.csv` file type.
-    case csv
-
     /// A `.json` file type.
     case json
 
     /// The file extension type to use.
     var fileExtension: String {
         switch self {
-        case .csv:
-            "csv"
         case .json:
             "json"
         }
