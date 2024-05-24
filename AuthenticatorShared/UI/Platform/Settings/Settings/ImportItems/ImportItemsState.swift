1bfa2882f571c21eb0f2ba9a3392eb50c76df7f0
Katherine Bertelsen <kbertelsen@bitwarden.com>
2024-05-24 10:14:08 -0500

    Cleanup

--- AuthenticatorShared/UI/Platform/Settings/Settings/ImportItems/ImportItemsState.swift
+++ AuthenticatorShared/UI/Platform/Settings/Settings/ImportItems/ImportItemsState.swift
@@ -5,7 +5,7 @@ struct ImportItemsState: Equatable {
     // MARK: Properties
 
     /// The currently selected file format type.
-    var fileFormat: ImportFormatType = .googleQr // TODO return to Bitwarden
+    var fileFormat: ImportFormatType = .googleQr
 
     /// A toast for views
     var toast: Toast?
