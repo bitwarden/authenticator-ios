1d714780535666641cb358aa7d2204eceee99538
Brant DeBow <brant@livefront.com>
2024-09-10 13:52:53 -0400

    Initial changes for importing SDK

--- AuthenticatorShared/Core/Auth/Services/KeychainService.swift
+++ AuthenticatorShared/Core/Auth/Services/KeychainService.swift
@@ -1,3 +1,4 @@
+import AuthenticatorSyncShared
 import Foundation
 
 // MARK: - KeychainService
@@ -110,3 +111,7 @@ class DefaultKeychainService: KeychainService {
         }
     }
 }
+
+// MARK: - AuthenticatorKeychainService
+
+extension DefaultKeychainService: AuthenticatorKeychainService {}
