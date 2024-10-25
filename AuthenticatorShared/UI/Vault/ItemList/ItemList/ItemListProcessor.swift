85c59a83dc55bc884f7d464bc7637a7d41ff604f
Brant DeBow <brant@livefront.com>
2024-10-25 12:29:40 -0400

    Updated per PR review: removed BW shortening and renamed hash function

--- AuthenticatorShared/UI/Vault/ItemList/ItemList/ItemListProcessor.swift
+++ AuthenticatorShared/UI/Vault/ItemList/ItemList/ItemListProcessor.swift
@@ -88,9 +88,9 @@ final class ItemListProcessor: StateProcessor<ItemListState, ItemListAction, Ite
                 else { return }
                 await generateAndCopyTotpCode(totpKey: totpKey)
             }
-        case let .moveToBWPressed(item):
+        case let .moveToBitwardenPressed(item):
             guard case .totp = item.itemType else { return }
-            await moveItemToBW(item: item)
+            await moveItemToBitwarden(item: item)
         case .refresh:
             await streamItemList()
         case let .search(text):
@@ -166,12 +166,12 @@ final class ItemListProcessor: StateProcessor<ItemListState, ItemListAction, Ite
         }
     }
 
-    /// Store the item in the shared sync data store as a temporary item and deeplink to the BW app to
+    /// Store the item in the shared sync data store as a temporary item and deeplink to the Bitwarden app to
     /// let the user choose where to store it.
     ///
     /// - Parameter item: the item to be moved.
     ///
-    private func moveItemToBW(item: ItemListItem) async {
+    private func moveItemToBitwarden(item: ItemListItem) async {
         guard await services.configService.getFeatureFlag(.enablePasswordManagerSync),
               let application = services.application,
               application.canOpenURL(ExternalLinksConstants.passwordManagerScheme)
