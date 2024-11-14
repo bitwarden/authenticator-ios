bc0ac1223736f421eb31d5ab83acd10c01b3bbc2
Brant DeBow <brant@livefront.com>
2024-11-14 11:55:47 -0500

    [BITAU-210] Update copy text and icon for "Sync with the Bitwarden app" CTA Card

--- AuthenticatorShared/UI/Vault/ItemList/ItemList/ItemListView.swift
+++ AuthenticatorShared/UI/Vault/ItemList/ItemList/ItemListView.swift
@@ -154,10 +154,10 @@ private struct SearchableItemListView: View { // swiftlint:disable:this type_bod
     private var itemListCardSync: some View {
         ItemListCardView(
             bodyText: Localizations
-                .inOrderToViewAllOfYourVerificationCodesYoullNeedToAllowForSyncingOnAllOfYourAccounts,
+                .allowAuthenticatorAppSyncingInSettingsToViewAllYourVerificationCodesHere,
             buttonText: Localizations.takeMeToTheAppSettings,
             leftImage: {
-                Image(decorative: Asset.Images.bwLogo)
+                Image(decorative: Asset.Images.syncArrow)
                     .foregroundColor(Asset.Colors.primaryBitwardenLight.swiftUIColor)
                     .frame(width: 24, height: 24)
             },
