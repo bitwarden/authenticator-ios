ee92796c9f0e9528f51bd625bd1c8a7853760042
Brant DeBow <brant@livefront.com>
2024-11-21 16:36:46 -0500

    [BITAU-216] Change Move to Copy in Long Press Menu

--- AuthenticatorShared/UI/Vault/ItemList/ItemList/ItemListView.swift
+++ AuthenticatorShared/UI/Vault/ItemList/ItemList/ItemListView.swift
@@ -239,7 +239,7 @@ private struct SearchableItemListView: View {
                                 await store.perform(.moveToBitwardenPressed(item))
                             } label: {
                                 HStack(spacing: 4) {
-                                    Text(Localizations.moveToBitwarden)
+                                    Text(Localizations.copyToBitwarden)
                                     Spacer()
                                     Image(decorative: Asset.Images.rightArrow)
                                         .imageStyle(.accessoryIcon(scaleWithFont: true))
